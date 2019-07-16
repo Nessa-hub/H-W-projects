9989acb4c66fdeda7e1ca8a784047691876a30ea
Nessa <vmgbekor@gmail.com>
2019-07-16 18:09:33 +0000

    Initial commit

---
+++ strings1.py
@@ -0,0 +1,44 @@
+#String with Double Quotation Marks
+print("Today is a good day")
+
+
+#String with Single Quotation Marks
+print('Tomorrow is Wednesday')
+
+
+#Multi_line Strings
+multi_line = """Today is a good day,
+tomorrow is Wednesday,
+tomorrow will be a good day. """
+print(multi_line)
+
+
+#Single_line Strings
+single_line = "Today is a good day, tomorrow is Wednesday, tomorrow will be a good day."
+print(single_line)
+
+
+#Checking the length of a string
+x = "yellow"
+print(len(x))
+
+
+#String concatenation
+str1 = "Today is Tuesday"
+str2 = "\ so tomorrow will be Wednesday."
+str3 = str1 + str2
+print(str3)
+
+
+#Print Output
+var1 = "y'ello"
+var2 = "world :)"
+print(var1, var2)
+
+
+#String Arrays
+str = "bazinga"
+print(str[2:6])
+
+
+
